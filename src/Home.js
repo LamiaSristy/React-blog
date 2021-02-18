db83803ae98b6f175075de55eaf67ac6414e102a
lamiasristy <lamiasristy@gmail.com>
2021-02-18 17:38:54 +0100

    Loading and Error handling

--- src/Home.js
+++ src/Home.js
@@ -3,21 +3,50 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null)
+  const [isPending, setIsPending] = useState(true)
+  const [error, setError] = useState(null);
  
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      // console.log(data);
-      setBlogs(data);
-    })
+      .then(res => {
+        // console.log(res);
+        if(!res.ok){
+          throw Error('Could not find the data.');
+        }
+        return res.json();
+      })
+      .then(data => {
+        // console.log(data);
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err => {
+        // console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      })
   }, []);
 
+  // useEffect(() =>{
+  //   setTimeout(() => {
+  //     fetch('http://localhost:8000/blogs')
+  //     .then(res => {
+  //       return res.json();
+  //     })
+  //     .then(data => {
+  //       // console.log(data);
+  //       setBlogs(data);
+  //       setIsPending(false);
+  //     })
+  //   }, 1000)
+  // }, []) 
+
   return (
-    <div className="home">    
-      { blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+    <div className="home">
+      { error && <div>{error}</div>} 
+      { isPending && <div>Loading...</div>}
+      { blogs && <BlogList blogs={blogs} title="All Blogs"/>} 
     </div>
   );
 }
